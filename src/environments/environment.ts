98f425f5dae88045a9b9391c083b6a9337a9e333
Daniel Zapata <90068737+daniel7apata@users.noreply.github.com>
2023-11-19 20:21:21 -0500

    Add files via upload

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment={
     production:false,
     base:"http://localhost:8085"
+    //base:"http://3.88.148.147:8085"
 }
 //PASO 1
 //primero empieza por aca, fijate que el puerto es el mismo que el del backend, ahora ve a model/medicine.ts
